d0754a12e6f705a546d74641f67ea2d0621093d6
lyric <tiannianshou@gmail.com>
2016-04-19 20:02:17 +0800

    Add binary convertion

--- binary.go
+++ binary.go
@@ -1,9 +1,82 @@
 package lib
 
-// B Provide a []byte convert operation
+import (
+	"encoding/binary"
+
+	"bytes"
+)
+
+// B Provide a []byte conversions
 type B []byte
 
-// ToString Convert []byte to string
-func (b B) ToString() string {
+// String Convert []byte to string
+func (b B) String() string {
 	return string(b)
+}
+
+// Buffer Convert []byte to buffer
+func (b B) Buffer() *bytes.Buffer {
+	return bytes.NewBuffer(b)
+}
+
+func (b B) read(v interface{}) error {
+	buf := bytes.NewReader(b)
+	return binary.Read(buf, binary.LittleEndian, v)
+}
+
+// Int64 Convert []byte to int64
+func (b B) Int64() (int64, error) {
+	var v int64
+	err := b.read(&v)
+	if err != nil {
+		return 0, err
+	}
+	return v, nil
+}
+
+// DefaultInt64 Convert []byte to int64
+func (b B) DefaultInt64(defaultVal int64) int64 {
+	v, err := b.Int64()
+	if err != nil {
+		return defaultVal
+	}
+	return v
+}
+
+// Uint64 Convert []byte to uint64
+func (b B) Uint64() (uint64, error) {
+	var v uint64
+	err := b.read(&v)
+	if err != nil {
+		return 0, err
+	}
+	return v, nil
+}
+
+// DefaultUint64 Convert []byte to uint64
+func (b B) DefaultUint64(defaultVal uint64) uint64 {
+	v, err := b.Uint64()
+	if err != nil {
+		return defaultVal
+	}
+	return v
+}
+
+// Float64 Convert []byte to float64
+func (b B) Float64() (float64, error) {
+	var v float64
+	err := b.read(&v)
+	if err != nil {
+		return 0, err
+	}
+	return v, nil
+}
+
+// DefaultFloat64 Convert []byte to float64
+func (b B) DefaultFloat64(defaultVal float64) float64 {
+	v, err := b.Float64()
+	if err != nil {
+		return defaultVal
+	}
+	return v
 }
--- binary_test.go
+++ binary_test.go
@@ -1,16 +1,45 @@
 package lib_test
 
 import (
+	"bytes"
+	"encoding/binary"
 	"testing"
 
 	"gopkg.in/LyricTian/lib.v1"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestBinary(t *testing.T) {
-	v := []byte("123")
-	bv := lib.B(v)
-	if v := bv.ToString(); v != "123" {
-		t.Error("Convert error:", v)
-		return
-	}
+	Convey("Subject: binary convertions test", t, func() {
+		var buf bytes.Buffer
+		err := binary.Write(&buf, binary.LittleEndian, int64(100))
+		So(err, ShouldBeNil)
+		v := buf.Bytes()
+		Convey("String Test", func() {
+			sv := []byte("foo")
+			So(lib.B(sv).String(), ShouldEqual, "foo")
+		})
+		Convey("Buffer Test", func() {
+			So(lib.B(v).Buffer().Len(), ShouldBeGreaterThan, 0)
+		})
+		Convey("Int64 Test", func() {
+			iv, err := lib.B(v).Int64()
+			So(err, ShouldBeNil)
+			So(iv, ShouldEqual, 100)
+			So(lib.B(v).DefaultInt64(0), ShouldEqual, 100)
+		})
+		Convey("Uint64 Test", func() {
+			iv, err := lib.B(v).Uint64()
+			So(err, ShouldBeNil)
+			So(iv, ShouldEqual, 100)
+			So(lib.B(v).DefaultUint64(0), ShouldEqual, 100)
+		})
+		Convey("Float64 Test", func() {
+			iv, err := lib.B(v).Float64()
+			So(err, ShouldBeNil)
+			So(iv, ShouldBeGreaterThan, 0)
+			So(lib.B([]byte("10")).DefaultFloat64(10), ShouldEqual, 10)
+		})
+	})
 }
