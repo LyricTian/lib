1a9d0894953ee236801555a5382de2f068a9c31f
lyric <tiannianshou@gmail.com>
2016-05-12 11:32:57 +0800

    Modify mongo,add db name

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -8,18 +8,27 @@ import (
 // InitHandler 初始化Handler
 // url MongoDB连接路径
 func InitHandler(url string) (*Handler, error) {
+	return InitHandlerWithDB(url, "")
+}
+
+// InitHandlerWithDB 初始化Handler(使用指定的数据库名称)
+// url MongoDB连接路径
+// dbName 数据库名称
+func InitHandlerWithDB(url, dbName string) (*Handler, error) {
 	session, err := mgo.Dial(url)
 	if err != nil {
 		return nil, err
 	}
 	return &Handler{
 		session: session,
+		dbName:  dbName,
 	}, nil
 }
 
 // Handler 提供MongoDB的数据库操作
 type Handler struct {
 	session *mgo.Session
+	dbName  string
 }
 
 // Session 获取当前会话
@@ -32,28 +41,39 @@ func (h *Handler) CloneSession() *mgo.Session {
 	return h.session.Clone()
 }
 
-// DBHandle 使用新的DB处理带有回调函数的集合,处理完成之后将关闭该会话(处理时将使用克隆的会话)
-func (h *Handler) DBHandle(dbName, cName string, handle func(c *mgo.Collection)) {
-	session := h.CloneSession()
-	defer session.Close()
-	handle(session.DB(dbName).C(cName))
+// DB 获取当前数据库实例
+func (h *Handler) DB() *mgo.Database {
+	return h.session.DB(h.dbName)
+}
+
+// DBWithName 获取指定的数据库实例
+// dbName 数据库名称
+func (h *Handler) DBWithName(dbName string) *mgo.Database {
+	return h.session.DB(dbName)
 }
 
 // CHandle 使用默认DB处理带有回调函数的集合,处理完成之后将关闭该会话(处理时将使用克隆的会话)
 func (h *Handler) CHandle(cName string, handle func(c *mgo.Collection)) {
 	session := h.CloneSession()
 	defer session.Close()
-	handle(session.DB("").C(cName))
+	handle(session.DB(h.dbName).C(cName))
 }
 
-// DB 使用新的DB处理集合(使用当前会话)
-func (h *Handler) DB(dbName, cName string) *mgo.Collection {
-	return h.session.DB(dbName).C(cName)
+// CHandleWithDB 使用新的DB处理带有回调函数的集合,处理完成之后将关闭该会话(处理时将使用克隆的会话)
+func (h *Handler) CHandleWithDB(dbName, cName string, handle func(c *mgo.Collection)) {
+	session := h.CloneSession()
+	defer session.Close()
+	handle(session.DB(dbName).C(cName))
 }
 
 // C 使用默认DB处理集合(使用当前会话)
 func (h *Handler) C(cName string) *mgo.Collection {
-	return h.session.DB("").C(cName)
+	return h.session.DB(h.dbName).C(cName)
+}
+
+// CWithDB 使用新的DB处理集合(使用当前会话)
+func (h *Handler) CWithDB(dbName, cName string) *mgo.Collection {
+	return h.session.DB(dbName).C(cName)
 }
 
 // IncrID 返回一个自增ID
--- mongo/mongo_test.go
+++ mongo/mongo_test.go
@@ -5,18 +5,16 @@ import (
 )
 
 func TestIncrID(t *testing.T) {
-	handler, err := InitHandler("mongodb://192.168.33.70:27017/test")
+	handler, err := InitHandlerWithDB("mongodb://admin:123456@192.168.33.70:27017", "test")
 	if err != nil {
-		t.Error(err)
-		return
+		t.Fatal(err)
 	}
 	defer handler.Session().Close()
 	id, err := handler.IncrID("foo")
 	if err != nil {
-		t.Error(err)
-		return
+		t.Fatal(err)
 	}
-	if id <= 0 {
-		t.Error("Get id error:", id)
+	if id == 0 {
+		t.Fatal("Get id error:", id)
 	}
 }
